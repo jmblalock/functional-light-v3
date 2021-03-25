c6605eff729291f2056136a48e1004ff2e3d37e7
jmblalock <justin.m.blalock@gmail.com>
2021-03-25 14:07:26 -0500

    list-operations-exercise

--- lists/ex.fixed.js
+++ lists/ex.fixed.js
@@ -2,26 +2,29 @@
 
 // function foo() { return 42; }
 // function bar() { return 10; }
-function add(x,y) { return x + y; }
+function add(x, y) {
+  return x + y;
+}
 // add( foo(), bar() );		// 52
-function add2(fn1,fn2) { return add( fn1(), fn2() ); }
+function add2(fn1, fn2) {
+  return add(fn1(), fn2());
+}
 // add2( foo, bar );		// 52
 
 function constant(v) {
-	return function f(){
-		return v;
-	};
+  return function f() {
+    return v;
+  };
 }
 // add2( constant(42), constant(10) );		// 52
 
-
 // with reduce:
 function addn(fns) {
-	return fns.reduce(function reducer(composedFn,fn){
-		return function f(){
-			return add2(composedFn,fn);
-		};
-	})();
+  return fns.reduce(function reducer(composedFn, fn) {
+    return function f() {
+      return add2(composedFn, fn);
+    };
+  })();
 }
 
 // with recursion:
@@ -52,21 +55,20 @@ function addn(fns) {
 // 	return add2(fns[0],fns[1]);
 // }
 
-
-var vals = [7,4,8,0,10,7,3,2,5,9,12,6,4,1,7,8];
+var vals = [7, 4, 8, 0, 10, 7, 3, 2, 5, 9, 12, 6, 4, 1, 7, 8];
 
 addn(
-	vals
-	.reduce(function reducer(a,v){
-		if (!~a.indexOf(v)) return a.concat(v);
-		return a;
-	},[])
-	//.filter(function(v,i,arr){
-	//    return i == arr.indexOf(v);
-	//})
-	.filter(function filterer(v){
-		return v % 2 == 0;
-	})
-	.map(constant)
+  vals
+    .reduce(function reducer(a, v) {
+      if (!~a.indexOf(v)) return a.concat(v);
+      return a;
+    }, [])
+    //.filter(function(v,i,arr){
+    //    return i == arr.indexOf(v);
+    //})
+    .filter(function filterer(v) {
+      return v % 2 == 0;
+    })
+    .map(constant)
 );
 // 42
--- lists/ex.js
+++ lists/ex.js
@@ -1,3 +1,84 @@
 "use strict";
 
-// Put your code here! :)
+function one() {
+  return 1;
+}
+
+function two() {
+  return 2;
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(one(), two())); // 3
+
+function add2(fn1, fn2) {
+  return add(fn1(), fn2());
+}
+
+console.log(add2(one, two)); // 3
+
+function three(v) {
+  return function f() {
+    return v;
+  };
+}
+
+console.log(add2(three(1), three(2))); // 3
+
+// with recursion:
+function addn([fn0, fn1, ...rest]) {
+  if (rest.length == 0) return add2(fn0, fn1);
+
+  return addn([
+    function f() {
+      return add2(fn0, fn1);
+    },
+    ...rest,
+  ]);
+}
+
+var vals = [7, 4, 8, 0, 10, 7, 3, 2, 5, 9, 12, 6, 4, 1, 7, 8];
+
+// with reduce:
+// function addn(fns) {
+//   return fns.reduce(function reducer(composedFn, fn) {
+//     return function f() {
+//       return add2(composedFn, fn);
+//     };
+//   })();
+// }
+
+// with iteration:
+// function addn(fns) {
+// 	while (fns.length > 2) {
+// 		let [fn0,fn1,...rest] = fns;
+// 		fns = [
+// 			function f(){
+// 				return add2(fn0,fn1);
+// 			},
+// 			...rest
+// 		];
+// 	}
+// 	return add2(fns[0],fns[1]);
+// }
+
+console.log(
+  addn(
+    vals
+      .reduce(function reducer(a, v) {
+        if (!~a.indexOf(v)) return a.concat(v);
+        return a;
+      }, [])
+      //.filter(function(v,i,arr){
+      //    return i == arr.indexOf(v);
+      //})
+      .filter(function filterer(v) {
+        return v % 2 == 0;
+      })
+      .map(three)
+  )
+);
+// 42
